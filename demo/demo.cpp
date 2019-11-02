9253387d05fd855824260067c13c2e75f3d0bb50
solopointer <solopointer1202@gmail.com>
2019-11-03 00:21:00 +0800

    update protobuf

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,7 +1,12 @@
 #include <string>
+#include "demo.pb.h"
+using namespace demo;
 
 
 int main()
 {
+    demo_response response;
+    response.set_name("gservice");
+    demo_request request;
     return 0;
 }
