2d8cf8b086900916176203d8e0f68773be60a7e6
solopointer <solopointer1202@gmail.com>
2019-11-03 18:02:21 +0800

    submodule

--- include/gservice.h
+++ include/gservice.h
@@ -1,1 +1,26 @@
 #include <vector>
+#include "gpendingpool/include/gpendingpool.h"
+#include "ghead/include/ghead.h"
+
+
+namespace galois::gservice
+{
+
+template < template <class> query_content, 
+    template <class> thread_content, 
+    class process_content >
+class gservice
+{
+    typedef typename thread_content<query_content> tcontent;
+    typedef typename query_content<process_content, thread_content> qcontent; 
+public:
+    explicit gservice(std::string name)
+    {
+
+    }
+    virtual ~gservice(){}
+}
+}
+
+
+
